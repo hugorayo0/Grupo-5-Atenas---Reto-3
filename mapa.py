d9d736f65148453a99de8dbffea0bec5ee7c7190
ibailopeez <ibailopeez>
2026-01-21 09:29:16 +0100

    Commit de la Primera versión del mapa.py

---
+++ mapa.py
@@ -0,0 +1,14 @@
+# Este python consiste en un mapa de la distribución de la fábrica de coches.
+print("""         ______________________________________________
+        |                                              |      
+        |     XXXXXXXXXXXXXXX        CARROCERIA        |
+        |                  XX                          |
+        |     XXXXXXXXXXXXXXX        RUEDAS            |
+        |     XX                                       |
+        |     XXXXXXXXXXXXXXX        VENTANAS          |
+        |                  XX                          |
+        |     XXXXXXXXXXXXXXX        MOTOR             |
+        |     XX                                       |
+        |     XXXXXXXXXXXXXXX        LUCES             |
+        |______________________________________________|
+""")
