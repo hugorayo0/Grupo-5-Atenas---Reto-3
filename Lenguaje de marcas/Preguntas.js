5240531c130c50a073860b984ca2f94851e9939d
ibailoopez <102789899+ibai200@users.noreply.github.com>
2026-01-28 14:09:19 +0100

    Commit final de los archivos de Lenguaje de marcas

---
+++ Lenguaje de marcas/Preguntas.js
@@ -0,0 +1,66 @@
+// Preguntas.js realizado por Ibai
+const TOTAL_PREGUNTAS = 20
+const preguntasData = {
+  "preguntas": {
+    "pregunta1": {
+      "correcta": "Autónomo"
+    },
+    "pregunta2": {
+      "correcta": "Dependiente"
+    },
+    "pregunta3": {
+      "correcta": "Laborales"
+    },
+    "pregunta4": {
+      "correcta": "Superior"
+    },
+    "pregunta5": {
+      "correcta": "Verdadero"
+    },
+    "pregunta6": {
+      "correcta": "NormativaEuropea"
+    },
+    "pregunta7": {
+      "correcta": "Siempre"
+    },
+    "pregunta8": {
+      "correcta": "Fijo_discontinuo"
+    },
+    "pregunta9": {
+      "correcta": "Producción"
+    },
+    "pregunta10": {
+      "correcta": "Sustitución"
+    },
+    "pregunta11": {
+      "correcta": "Alternancia"
+    },
+    "pregunta12": {
+      "correcta": "UnAño"
+    },
+    "pregunta13": {
+      "correcta": "Jubilación"
+    },
+    "pregunta14": {
+      "correcta": "16_permiso"
+    },
+    "pregunta15": {
+      "correcta": "15minutos"
+    },
+    "pregunta16": {
+      "correcta": "15días"
+    },
+    "pregunta17": {
+      "correcta": "30"
+    },
+    "pregunta18": {
+      "correcta": "Min30_sin_max"
+    },
+    "pregunta19": {
+      "correcta": "Indemnizaciones"
+    },
+    "pregunta20": {
+      "correcta": "Paro"
+    }
+  }
+};
---
+++ Lenguaje de marcas/ScriptNav.js
@@ -0,0 +1,16 @@
+// ScriptNav.js realizado por ibai
+function Help() {
+    window.location.href = "help.html";
+}
+function RRHH() {
+    window.location.href = "RRHH.html";
+}
+function Fabrica() {
+    window.location.href = "fabrica.html";
+}
+function index() {
+    window.location.href = "index.html";
+}
+function FAQ() {
+    window.location.href = "FAQ.html";
+}
