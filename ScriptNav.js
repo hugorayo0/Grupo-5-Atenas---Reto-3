728bc8183c1b8a5a8f353ee11aa47ad863acfde4
ibailopeez <ibailopezbastida@gmail.com>
2026-01-28 11:25:34 +0100

    Actualizacion de archivos

---
+++ ScriptNav.js
@@ -0,0 +1,15 @@
+function Help() {
+    window.location.href = "help.html";
+}
+function RRHH() {
+    window.location.href = "RRHH.html";
+}
+function Fabrica() {
+    window.location.href = "fabrica.html";
+}
+function index() {
+    window.location.href = "index.html";
+}
+function FAQ() {
+    window.location.href = "FAQ.html";
+}
